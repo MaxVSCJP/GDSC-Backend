fbaa5df3f4f3c9f516124687d4f26be1f21b22f5
Hansonket <139464736+MaxVSCJP@users.noreply.github.com>
2024-07-24 20:12:18 +0300

    Added Day 5 Countdown Timer

--- Exercise 5 - 07-18-2024/script.js
+++ Exercise 5 - 07-18-2024/script.js
@@ -1,6 +1,35 @@
 
 const time = document.getElementById("timer");
+const timeKeeper = document.getElementById("TimeKeeper");
+const countdown = document.createElement("p");
+let nowTime;
+let intervalID;
 
-function makeCountdown(time){
-    document.createElement()
+time.addEventListener("keypress", (eve) => {
+    if(eve.key === "Enter"){
+        countdown.textContent = time.value;
+        timeKeeper.appendChild(countdown);
+        nowTime = countdown.textContent;
+        countTime()
+    }
+
+});
+
+function countTime(){
+intervalID = setInterval(() => {
+    nowTime = nowTime - 1;
+    countdown.textContent = nowTime;
+    timeKeeper.appendChild(countdown);
+}, 1000);
+
+
+let checkTime = setInterval(() => {
+    if(nowTime === 0){
+        clearInterval(intervalID);
+        alert("Time's Up");
+        console.log("Time's Up");
+        clearInterval(checkTime);
+    }
+}, 100);
+
 }
